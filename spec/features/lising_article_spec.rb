3e5b41b6e02852f56b434e252d144e4d2cc5313c
Tjitske Lohstroh <tjitske@lohstroh.nl>
2017-02-07 15:02:15 +0100

    Show New Article button to signed in users only

--- spec/features/lising_article_spec.rb
+++ spec/features/lising_article_spec.rb
@@ -2,11 +2,45 @@ require "rails_helper"
 
 RSpec.feature "Listing Articles" do
   before do
-    john = User.create!(email: "me@example.com", password: "password")
-    # no need to log in here!
+    @john = User.create!(email: "me@example.com", password: "password")
+
     # create 2 Articles
-    @article1 = Article.create(title: "The first article", body: "Lorem ipsum dolor sit amet, consectetur.", user: john)
-    @article2 = Article.create(title: "The second article", body: "Pellentesque ac ligula in tellus feugiat.", user: john)
+    @article1 = Article.create(title: "The first article", body: "Lorem ipsum dolor sit amet, consectetur.", user: @john)
+    @article2 = Article.create(title: "The second article", body: "Pellentesque ac ligula in tellus feugiat.", user: @john)
+  end
+
+  scenario "with articles created and users not signed in." do
+
+    # list the 2 Articles
+    visit "/"
+
+    # expect both article titles and bodies to be present
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+    expect(page).not_to have_link("New Article")
+
+  end
+
+  scenario "with articles created and users signed in." do
+    login_as(@john)
+    # list the 2 Articles
+    visit "/"
+
+    # expect both article titles and bodies to be present
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+    expect(page).to have_link("New Article")
+
   end
 
   scenario "A user lists all artiles" do
